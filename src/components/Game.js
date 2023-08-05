e9c9dc8cc18f7646436fdf6c62f96228a136ebed
XecellencWeb <128531293+XecellencWeb@users.noreply.github.com>
2023-08-05 16:00:35 +0100

    Update Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -14,7 +14,7 @@ function Game(Props) {
     <div className='game'>
         <img src={picture} alt={value}/>
         <div className="spaceBetween">
-        <span className="value"><Link to={expected}>{value}</Link></span><HeartFill className={thegame.likedby.includes(user._id)?'red':'heart'} size={20} onClick={()=>{thegame.likedby.includes(user._id)?unLike(thegame._id,user._id):onLike(thegame._id,user._id)}}/>
+        <span className="value"><Link to={expected}>{value}</Link></span><HeartFill className={thegame && thegame.likedby.includes(user._id)?'red':'heart'} size={20} onClick={()=>{thegame && thegame.likedby.includes(user._id)?unLike(thegame._id,user._id):onLike(thegame._id,user._id)}}/>
         </div>
     </div>
   )
